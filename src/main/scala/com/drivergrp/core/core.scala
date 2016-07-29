1f8a7112150b74175dc87c9df09ec796f6c3fdeb
vlad <vlad@drivergrp.com>
2016-07-29 13:48:09 -0700

    Ordering for Id and Name

--- src/main/scala/com/drivergrp/core/core.scala
+++ src/main/scala/com/drivergrp/core/core.scala
@@ -25,13 +25,14 @@ package object core {
   object Id {
     def apply[Tag](value: Long) = value.asInstanceOf[Id[Tag]]
   }
-
-  implicit def idEqual[T]: Equal[Id[T]] = Equal.equal[Id[T]](_ == _)
+  implicit def idEqual[T]: Equal[Id[T]]       = Equal.equal[Id[T]](_ == _)
+  implicit def idOrdering[T]: Ordering[Id[T]] = Ordering.by(identity)
 
   type Name[+Tag] = String @@ Tag
   object Name {
     def apply[Tag](value: String) = value.asInstanceOf[Name[Tag]]
   }
 
-  implicit def nameEqual[T]: Equal[Name[T]] = Equal.equal[Name[T]](_ == _)
+  implicit def nameEqual[T]: Equal[Name[T]]       = Equal.equal[Name[T]](_ == _)
+  implicit def nameOrdering[T]: Ordering[Name[T]] = Ordering.by(identity)
 }

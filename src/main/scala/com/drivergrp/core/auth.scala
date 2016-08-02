f97ab65634b5b88e3e42e10f9f4b0923d88a192b
vlad <vlad@drivergrp.com>
2016-08-02 16:45:06 -0700

    More abstract auth model and File service definition

--- src/main/scala/com/drivergrp/core/auth.scala
+++ src/main/scala/com/drivergrp/core/auth.scala
@@ -2,64 +2,19 @@ package com.drivergrp.core
 
 object auth {
 
-  final case class FullName[+T](firstName: Name[T], middleName: Name[T], lastName: Name[T])
-
-  final case class Email(username: String, domain: String) {
-    override def toString = username + "@" + domain
-  }
+  trait Permission
 
   trait Role {
     val id: Id[Role]
     val name: Name[Role]
 
-    def canEditReport: Boolean    = false
-    def canSignOffReport: Boolean = false
-    def canAssignRoles: Boolean   = false
+    def hasPermission(permission: Permission): Boolean = false
   }
 
-  case object ObserverRole extends Role {
-    val id   = Id(1L)
-    val name = Name("observer")
+  trait User {
+    def id: Id[User]
+    def roles: Set[Role]
   }
-
-  case object PatientRole extends Role {
-    val id   = Id(2L)
-    val name = Name("patient")
-  }
-
-  case object CuratorRole extends Role {
-    val id   = Id(3L)
-    val name = Name("curator")
-
-    override def canEditReport: Boolean = true
-  }
-
-  case object PathologistRole extends Role {
-    val id   = Id(4L)
-    val name = Name("pathologist")
-
-    override def canEditReport: Boolean    = true
-    override def canSignOffReport: Boolean = true
-  }
-
-  case object AdministratorRole extends Role {
-    val id   = Id(5L)
-    val name = Name("administrator")
-
-    override def canEditReport: Boolean    = true
-    override def canSignOffReport: Boolean = true
-    override def canAssignRoles: Boolean   = true
-  }
-
-  final case class Avatar(id: Id[Avatar], name: Name[Avatar])
-
-  final case class User(id: Id[User], name: FullName[User], email: Email, avatar: Option[Avatar], roles: Set[Role])
-
-  val TestUser = User(Id[User](1L),
-                      FullName[User](Name("James"), Name("Dewey"), Name("Watson")),
-                      Email("j.watson", "uchicago.edu"),
-                      Some(Avatar(Id[Avatar](1L), Name[Avatar]("Coolface"))),
-                      Set(PathologistRole))
 
   final case class Macaroon(value: String)
 
@@ -73,13 +28,15 @@ object auth {
 
     val AuthenticationTokenHeader = "WWW-Authenticate"
 
-    def authorize(role: Role): Directive1[Id[User]] = {
+    type UserExtractor = AuthToken => Option[User]
+
+    def authorize(role: Role)(implicit userExtractor: UserExtractor): Directive1[Id[User]] = {
       headerValueByName(AuthenticationTokenHeader).flatMap { tokenValue =>
         val token = AuthToken(Base64[Macaroon](tokenValue))
 
-        extractUser(token) match {
+        userExtractor(token) match {
           case Some(user) =>
-            if (user.roles.contains(role)) provide(user.id)
+            if (user.roles.contains(role)) provide(user.id: Id[User])
             else reject(ValidationRejection(s"User does not have the required ${role.name} role"))
           case None =>
             reject(ValidationRejection(s"Wasn't able to extract user for the token provided"))
@@ -91,10 +48,6 @@ object auth {
       headerValueByName(AuthenticationTokenHeader).flatMap { token =>
         provide(AuthToken(Base64[Macaroon](token)))
       }
-    }
-
-    def extractUser(authToken: AuthToken): Option[User] = {
-      Some(TestUser)
     }
   }
 }
--- src/main/scala/com/drivergrp/core/file.scala
+++ src/main/scala/com/drivergrp/core/file.scala
@@ -1,12 +1,23 @@
 package com.drivergrp.core
 
+import akka.http.scaladsl.model.Uri
 import com.drivergrp.core.time.Time
 
 object file {
 
-  final case class Document(
-      id: Id[Document],
-      name: Name[Document],
+  final case class File(id: Id[File])
+
+  final case class FileLink(
+      id: Id[File],
+      name: Name[File],
+      location: Uri,
       additionDate: Time
   )
+
+  trait FileService {
+
+    def getFileLink(id: Id[File]): FileLink
+
+    def getFile(fileLink: FileLink): File
+  }
 }

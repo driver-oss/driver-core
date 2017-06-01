1f89848cf48a931ccc70fd08060b2112a788a43e
vlad <vlad@driver.xyz>
2017-06-01 14:23:33 -0700

    Scalafmt

--- src/main/scala/xyz/driver/core/database/PatchedHsqldbProfile.scala
+++ src/main/scala/xyz/driver/core/database/PatchedHsqldbProfile.scala
@@ -9,7 +9,7 @@ trait PatchedHsqldbProfile extends HsqldbDriver {
   override def defaultSqlTypeName(tmd: JdbcType[_], sym: Option[FieldSymbol]): String = tmd.sqlType match {
     case java.sql.Types.VARCHAR =>
       val size = sym.flatMap(_.findColumnOption[RelationalProfile.ColumnOption.Length])
-      size.fold("LONGVARCHAR")(l => if(l.varying) s"VARCHAR(${l.length})" else s"CHAR(${l.length})")
+      size.fold("LONGVARCHAR")(l => if (l.varying) s"VARCHAR(${l.length})" else s"CHAR(${l.length})")
     case _ => super.defaultSqlTypeName(tmd, sym)
   }
 }

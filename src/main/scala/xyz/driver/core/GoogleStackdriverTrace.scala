44923be843893086b2794b66c33147b9bc97486b
John St John <johnthesaintjohn@gmail.com>
2017-09-18 10:30:05 -0700

    try different header parse method

--- src/main/scala/xyz/driver/core/GoogleStackdriverTrace.scala
+++ src/main/scala/xyz/driver/core/GoogleStackdriverTrace.scala
@@ -9,14 +9,13 @@ import com.google.cloud.trace.core._
 import com.google.cloud.trace.grpc.v1.GrpcTraceConsumer
 import com.google.cloud.trace.sink.TraceSink
 import com.google.cloud.trace.v1.TraceSinkV1
-import com.google.cloud.trace.v1.consumer.TraceConsumer
+import com.google.cloud.trace.v1.consumer.{SimpleBufferingTraceConsumer, TraceConsumer}
 import com.google.cloud.trace.v1.producer.TraceProducer
-import com.google.cloud.trace.v1.consumer.SimpleBufferingTraceConsumer
 
 final case class GoogleStackdriverTrace(appName: String, projectId: String, clientSecretsFile:String, parentTraceHeaderStringOpt: Option[String]) {
 
   // Create the trace sink.
-  val traceProducer: TraceProducer = new TraceProducer
+  val traceProducer: TraceProducer = new TraceProducer()
   val traceConsumer: TraceConsumer = GrpcTraceConsumer
     .create("cloudtrace.googleapis.com",
       GoogleCredentials.fromStream(new FileInputStream(clientSecretsFile))
@@ -26,29 +25,25 @@ final case class GoogleStackdriverTrace(appName: String, projectId: String, clie
   val traceSink: TraceSink = new TraceSinkV1(projectId, traceProducer, flushableSink)
 
   // Create the tracer.
-  val spanContextFactory: SpanContextFactory = new SpanContextFactory(new ConstantTraceOptionsFactory(true, true))
+  val spanContextFactory: SpanContextFactory = new SpanContextFactory(new ConstantTraceOptionsFactory(true, false))
   val timestampFactory: TimestampFactory = new JavaTimestampFactory
   val spanContext: SpanContext = parentTraceHeaderStringOpt
     .fold(spanContextFactory.initialContext())(
-      parentTraceHeaderString => spanContextFactory.childContext(spanContextFactory.fromHeader(parentTraceHeaderString))
+      parentTraceHeaderString => spanContextFactory.fromHeader(parentTraceHeaderString)
     )
   val contextHandler: SpanContextHandler = new GrpcSpanContextHandler(spanContext)
+
   val tracer: Tracer = new SpanContextHandlerTracer(traceSink, contextHandler, spanContextFactory, timestampFactory)
-  contextHandler.attach(spanContext)
 
   // Create a span using the given timestamps.
   val context: TraceContext = tracer.startSpan(appName)
-  Trace.getTracer
-
-  val stackTraceBuilder: StackTrace.Builder = ThrowableStackTraceHelper.createBuilder(new Exception)
-  tracer.setStackTrace(context, stackTraceBuilder.build)
 
   def endSpan(): Unit = {
     tracer.endSpan(context)
     flushableSink.flush()
   }
   def headerValue: String = {
-    SpanContextFactory.toHeader(spanContext)
+    SpanContextFactory.toHeader(context.getHandle.getCurrentSpanContext)
   }
 }
 

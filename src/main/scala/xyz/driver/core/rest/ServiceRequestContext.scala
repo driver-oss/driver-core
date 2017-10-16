8a34b953fa480bfea7e80a46eb4de6b20b4bca68
Zach Smith <zach@driver.xyz>
2017-10-16 09:47:54 -0700

    Move AuthorizedServiceRequestContext into ServiceRequestContext.scala

--- src/main/scala/xyz/driver/core/rest/ServiceRequestContext.scala
+++ src/main/scala/xyz/driver/core/rest/ServiceRequestContext.scala
@@ -37,3 +37,26 @@ class ServiceRequestContext(val trackingId: String = generators.nextUuid().toStr
 
   override def toString: String = s"ServiceRequestContext($trackingId, $contextHeaders)"
 }
+
+class AuthorizedServiceRequestContext[U <: User](override val trackingId: String = generators.nextUuid().toString,
+                                                 override val contextHeaders: Map[String, String] =
+                                                   Map.empty[String, String],
+                                                 val authenticatedUser: U)
+    extends ServiceRequestContext {
+
+  def withPermissionsToken(permissionsToken: PermissionsToken): AuthorizedServiceRequestContext[U] =
+    new AuthorizedServiceRequestContext[U](
+      trackingId,
+      contextHeaders.updated(AuthProvider.PermissionsTokenHeader, permissionsToken.value),
+      authenticatedUser)
+
+  override def hashCode(): Int = 31 * super.hashCode() + authenticatedUser.hashCode()
+
+  override def equals(obj: Any): Boolean = obj match {
+    case ctx: AuthorizedServiceRequestContext[U] => super.equals(ctx) && ctx.authenticatedUser == authenticatedUser
+    case _                                       => false
+  }
+
+  override def toString: String =
+    s"AuthorizedServiceRequestContext($trackingId, $contextHeaders, $authenticatedUser)"
+}

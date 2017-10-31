5f3330ffd4df8d87b97d88789aced1b1b8f7410d
Zach Smith <zach@driver.xyz>
2017-10-30 20:27:22 -0700

    Add ExternalServiceException and use in ServiceTransport

--- src/main/scala/xyz/driver/core/rest/errors/serviceException.scala
+++ src/main/scala/xyz/driver/core/rest/errors/serviceException.scala
@@ -12,6 +12,10 @@ final case class InvalidActionException(override val message: String = "This act
 final case class ResourceNotFoundException(override val message: String = "Resource not found")
     extends ServiceException
 
+final case class ExternalServiceException(serviceName: String, serviceMessage: String) extends ServiceException {
+  override def message = s"Error while calling another service: $serviceMessage"
+}
+
 final case class ExternalServiceTimeoutException(
         override val message: String = "Another service took too long to respond")
     extends ServiceException

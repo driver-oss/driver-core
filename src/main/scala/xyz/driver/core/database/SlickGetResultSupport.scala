4dd8774c397fe97e94a5675f7742300afc1378f8
Zach Smith <zach@driver.xyz>
2017-11-14 17:28:26 -0800

    Rename to follow slick naming conventions

--- src/main/scala/xyz/driver/core/database/SlickGetResultSupport.scala
+++ src/main/scala/xyz/driver/core/database/SlickGetResultSupport.scala
@@ -6,24 +6,24 @@ import xyz.driver.core.time.Time
 import xyz.driver.core.{Id, Name}
 
 trait SlickGetResultSupport {
-  implicit def idGetResult[U]: GetResult[Id[U]] =
+  implicit def GetId[U]: GetResult[Id[U]] =
     GetResult(r => Id[U](r.nextString()))
-  implicit def idOptionGetResult[U]: GetResult[Option[Id[U]]] =
+  implicit def GetIdOption[U]: GetResult[Option[Id[U]]] =
     GetResult(_.nextStringOption().map(Id.apply[U]))
 
-  implicit def nameGetResult[U]: GetResult[Name[U]] =
+  implicit def GetName[U]: GetResult[Name[U]] =
     GetResult(r => Name[U](r.nextString()))
-  implicit def nameOptionGetResult[U]: GetResult[Option[Name[U]]] =
+  implicit def GetNameOption[U]: GetResult[Option[Name[U]]] =
     GetResult(_.nextStringOption().map(Name.apply[U]))
 
-  implicit val timeGetResult: GetResult[Time] =
+  implicit val GetTime: GetResult[Time] =
     GetResult(r => Time(r.nextTimestamp.getTime))
-  implicit val timeOptionGetResult: GetResult[Option[Time]] =
+  implicit val GetTimeOption: GetResult[Option[Time]] =
     GetResult(_.nextTimestampOption().map(t => Time(t.getTime)))
 
-  implicit val dateGetResult: GetResult[Date] =
+  implicit val GetDate: GetResult[Date] =
     GetResult(r => Date.fromJavaDate(r.nextDate()))
-  implicit val dateOptionGetResult: GetResult[Option[Date]] =
+  implicit val GetDateOption: GetResult[Option[Date]] =
     GetResult(_.nextDateOption().map(Date.fromJavaDate))
 }
 

05be443f86f18220579682de47882db22fe6943a
Sergey Nastich <sergey@driver.xyz>
2018-04-17 14:02:06 -0400

    Replace Id and Name unmarshallers with a single one which delegates to JsonFormats

--- src/main/scala/xyz/driver/core/json.scala
+++ src/main/scala/xyz/driver/core/json.scala
@@ -34,10 +34,11 @@ object json {
     }
   }
 
-  implicit def idUnmarshaller[T]: Unmarshaller[String, Id[T]] = Unmarshaller.strict {
-    case id if Try(UUID.fromString(id)).isSuccess => Id[T](id.toLowerCase)
-    case id                                       => Id[T](id)
-  }
+  implicit def paramUnmarshaller[T](implicit reader: JsonReader[T]): Unmarshaller[String, T] =
+    Unmarshaller.firstOf(
+      Unmarshaller.strict((JsString(_: String)) andThen reader.read),
+      stringToValueUnmarshaller[T]
+    )
 
   implicit def idFormat[T]: RootJsonFormat[Id[T]] = new RootJsonFormat[Id[T]] {
     def write(id: Id[T]) = JsString(id.value)
@@ -63,8 +64,6 @@ object json {
       case _                           => Unmatched
     }
   }
-
-  implicit def nameUnmarshaller[T]: Unmarshaller[String, Name[T]] = Unmarshaller.strict(Name[T])
 
   implicit def nameFormat[T] = new RootJsonFormat[Name[T]] {
     def write(name: Name[T]) = JsString(name.value)
